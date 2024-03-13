45de4fa6dbf6c4e56f1fe18376f1be7fe835a70a
Soniamy <marianna.marzullo@gmail.com>
2024-03-13 18:43:19 +0100

    Milestone3-Milestone4: Creazione di un nuovo progetto Vue 3,Installo Axios per gestire le chiamate API,Modifico il componente principale per effettuare la chiamata API.

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,21 +1,30 @@
 <script>
+import axios from 'axios';
 export default {
-    data() {
-        return {
-
-        };
-    },
-    methods: {
-
-    }
-}
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    axios.get('http://127.0.0.1:8000/api/posts').then((res) => {
+      console.log(res.data);
+      this.posts = res.data.results.data;
+    });
+  },
+  methods: {},
+};
 </script>
 
 <template>
-    <main>
-        MAIN
-    </main>
+  <main>
+    <h1>Tutti i Post!</h1>
+    <div>
+      <div v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </div>
+    </div>
+  </main>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
